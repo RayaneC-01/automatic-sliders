89fe21f3dcb91f4d9fc310493aa34aa0bc70eb51
Rayane <rchaabane20@gmail.com>
2024-05-03 23:56:16 +0200

    Ajout fichier JS

---
+++ script.js
@@ -0,0 +1,26 @@
+let slideIndex = 0;
+const slides = document.getElementsByClassName("carousel-slide");
+
+function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function nextSlide() {
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    showSlides();
+}
+
+function prevSlide() {
+    slideIndex--;
+    if (slideIndex < 1) { slideIndex = slides.length }
+    showSlides();
+}
+
+showSlides(); // Start the slideshow
